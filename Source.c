ff9d3c7b94f344853f911f7541600b87411f4dfd
eragonz <dzolotarev@cchgeu.ru>
2025-10-28 22:05:20 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+int main() {
+    int a, s;
+    char simvol;
+
+    printf("Введите угол SOT (a) в градусах: ");
+    scanf("%d", &a);
+    printf("Введите длину стороны OT (s): ");
+    scanf("%d", &s);
+    printf("Введите символ заполнения: ");
+    scanf(" %c", &simvol);
+
+    printf("\nТреугольник OST:\n");
+    printf("Угол SOT=%dгр., Сторона OT=%d\n\n", a, s);
+
+    int height = 10;
+    int num_strok = 0;
+
+    while (num_strok < height) {
+        int width = (s * (num_strok + 1)) / height;
+        if (width < 1) width = 1;
+
+        int max_width = s - width;
+        int smesh = max_width - (max_width * a) / 90;
+
+        int col = 0;
+        while (col < smesh) {
+            printf(" ");
+            col++;
+        }
+
+        col = 0;
+        while (col < width) {
+            printf("%c", simvol);
+            col++;
+        }
+
+        printf("\n");
+        num_strok++;
+    }
+
+    return 0;
+}
